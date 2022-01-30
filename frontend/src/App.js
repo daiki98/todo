09f4c0eafc9821c713d26352f2c86c0cbf683a67
daiki98 <zdymori@gmail.com>
2022-01-30 14:51:12 -0800

    added function renderItems

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -65,7 +65,39 @@ class App extends Component {
     );
   };
 
-
+  renderItems = () => {
+    const { viewCompleted } = this.state;
+    const newItems = this.state.todoList.filter(
+      (item) => item.completed == viewCompleted
+    );
+    return newItems.map((item) => (
+      <li
+        key={item.id}
+        className='list-group-item d-flex justify-content-between align-items-center'
+      >
+        <span
+          className={`todo-title mr-2 ${
+            this.state.viewCompleted ? 'completed-todo' : ''
+          }`}
+          title={item.description}
+        >
+          {item.title}
+        </span>
+        <span>
+          <button
+            className='btn btn-secondary mr-2'
+          >
+            Edit
+          </button>
+          <button
+            className='btn btn-danger'
+          >
+            Delete
+          </button>
+        </span>
+      </li>
+    ));
+  };
 }
 
 
